4fa12f6ae28dbef2a0e0798ad552dc518cd3f2c2
AarshRam <74356212+AarshRam@users.noreply.github.com>
2021-08-27 16:44:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
 video = createCapture(VIDEO);
 video.size(550,550);
@@ -10,6 +16,10 @@ poseNet.on('pose',gotPoses);
 
 function draw(){
     background('#808080');
+    fill('#F5F5F5');
+    stroke('#fff44f');
+    square(noseX , noseY , difference);
+    document.getElementById("square_sides").innerHTML = "The lenght of the square will be " + difference + " px";
 }
 
 function modelLoaded(){
@@ -19,5 +29,15 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length > 0){
     console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("Nose X = " + noseX + "and Nose Y = " + noseY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    leftWristX = results[0].pose.leftWrist.y;
+
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("LeftWristX = " + leftWristX + "RightWristX = " + rightWristX + "The differece in both wrist is " + difference);
 }
 }
